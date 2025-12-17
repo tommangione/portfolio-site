4afd632de7c0ea359d3c2ec8bd7d17f24939e2c1
Tom Mangione <tommangione@pm.me>
2025-12-17 11:40:04 -0600

    beginning work on a function that reads md and template files

--- src/main.py
+++ src/main.py
@@ -7,12 +7,45 @@ def main():
     # print("hello world")
     # example = TextNode("example time", "link", "www.example.com")
     # print(example.__repr__())
+
     try:
         recursive_copy("static", "public")
         print("Successfully copied static directory to public.")
     except Exception as e:
         print(f"Error: {e}")
         return
+    generate_page()
+
+
+def generate_page(from_path, template_path, dest_path):
+    from = ""
+    template = ""
+    print("Generating webpage...")
+    print(f"from path = {from_path}")
+    print(f"template path = {template_path}")
+    print(f"destination path = {dest_path}")
+
+def extract_title(markdown):
+    md_lines = markdown.splitlines()
+    title = ""
+    for line in md_lines:
+        if line.startswith("# "):
+            title = line[2:]
+            break
+    if title == "":
+        raise Exception("No title found.")
+    return title
+
+
+def deleteCheck():
+    choice = ""
+    print("This action will delete the 'public' folder within this directory.")
+    print("Would you like to proceed? Enter y to continue.")
+    choice = input()
+    if choice == "y":
+        shutil.rmtree(os.path.abspath("public"))
+    else:
+        raise Exception("Directory 'public' not cleared.")
 
 
 def recursive_copy(source, destination):
@@ -27,7 +60,8 @@ def recursive_copy(source, destination):
     if not os.path.exists(destination):
         os.makedirs(destination)
     else:
-        shutil.rmtree(destination)
+        deleteCheck()
+        os.makedirs(destination)
 
     for item in os.listdir(source):
         src_item = os.path.join(source, item)
